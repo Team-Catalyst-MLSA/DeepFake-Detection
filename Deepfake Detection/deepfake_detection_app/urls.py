5a70e03706593b53de6dd03e2a70e47846486def
Sreya E P <117088162+Sreya-E-P@users.noreply.github.com>
2024-07-16 16:56:00 +0530

    Delete Deepfake Detection directory

--- Deepfake Detection/deepfake_detection_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.deepfake_detection, name='deepfake_detection'),
-]
--- Deepfake Detection/deepfake_detection_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from deepfake_detection_app import views
-
-urlpatterns = [
-    path('', include('deepfake_detection_app.urls')),
-]
-
-
