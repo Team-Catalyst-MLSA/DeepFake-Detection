54e8f3d2857a3b27b5b685a295ecdc6513486969
Sreya E P <117088162+Sreya-E-P@users.noreply.github.com>
2024-07-17 17:34:35 +0530

    Initial commit

--- deepfake_detection_project/urls.py
+++ deepfake_detection_project/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.urls import path, include
-from deepfake_detection_app import views
 
 urlpatterns = [
     path('', include('deepfake_detection_app.urls')),
+    path('admin/', admin.site.urls),
+
 ]
 
 
